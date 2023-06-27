cf035c5eac52773bd4039ad0f2a6728ec7f4c3b2
Vitalii-Harkavenko <vitinfo22@gmail.com>
2023-06-27 14:05:19 +0300

    implemetring search

--- app/api/search-query/route.ts
+++ app/api/search-query/route.ts
@@ -1,8 +1,14 @@
 import { NextRequest } from "next/server"
+import searchQuery from "@/database/db"
 
 export async function GET(req: NextRequest) {
-	const { searchParams } = new URL(req.url)
-	const query = searchParams.get('query')
-	return new Response(JSON.stringify({ result: query }))
-
+	const { searchParams } = new URL(req.url);
+	const query = searchParams.get('query') || "";
+	const response = await searchQuery(query);
+	const result = () => {
+		if (Array.isArray(response) && response.length === 0)
+		return "Nothing found";
+		else return JSON.stringify(response);
+	}
+	return new Response(JSON.stringify({result: result()}))
 }
--- database/db.ts
+++ database/db.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-const executeQuery = async () => {
-  const result = await prisma.users.findMany();
+const searchQuery = async (query: string) => {
+  const result = await prisma.users.findMany({
+    where: {
+      username: {
+        equals: `${query}`
+      }
+    }
+  });
   return result;
 }
 
-export default executeQuery;
+export default searchQuery;

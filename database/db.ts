428f8be04147ece19add01d4d5a615c86636e25e
Vitalii-Harkavenko <vitinfo22@gmail.com>
2023-07-01 15:50:01 +0300

    added multiple search fuction

--- database/db.ts
+++ database/db.ts
@@ -2,10 +2,11 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const searchQuery = async (query: string) => {
+  const keywords = query.split(' ');
   const result = await prisma.users.findMany({
     where: {
       username: {
-        equals: `${query}`
+        in: keywords
       }
     }
   });
